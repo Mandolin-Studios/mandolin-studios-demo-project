48a77222ec121f39310b65aafbcfe5e06b52c195
Deverell Manning <deverellmanning321@gmail.com>
2025-01-21 20:35:06 -0700

    See through the leaves around player  - Improved Player Movement?  - Testing particle effect for player  - Added shader to make tree leaves transparent immediatly around player.  - Temporarily Disabled randomized tree.  - Removed old functionality for hiding trees that obscure the player  - Tree has separate sprites for trunk and leaves  - Added tree shadows / shader

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 240.0
+const ACCEL = 30.0
 const JUMP_VELOCITY = -400.0
 
 var health = 10
@@ -12,36 +13,17 @@ signal hide_object
 
 func _physics_process(delta: float) -> void:
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
 	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity += direction * ACCEL
+	
+	velocity.x *= 0.92
+	velocity.y *= 0.9
+	velocity = velocity.limit_length(SPEED)
 
 	move_and_slide()
+	
 
 #func hurt
-
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void	:
-	canopy_count += 1;
-	var hidden_opacity = 1/(3 * max(float(canopy_count), 1))
-	var object : Node2D = area.get_parent()
-	var opacity_tween = object.create_tween()
-	opacity_tween.tween_property(object, "modulate", Color(Color.WHITE, hidden_opacity), 0.5)
-
-
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	canopy_count = max(canopy_count - 1, 0);
-	var object : Node2D = area.get_parent()
-	var opacity_tween = object.create_tween()
-	opacity_tween.tween_property(object, "modulate", Color(Color.WHITE, 1), 0.5)
--- scenes/random_tree.gd
+++ scenes/random_tree.gd
@@ -4,9 +4,15 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	frame = [0,0,1,1,2,2,3,3,4,5].pick_random()
-	get_parent().scale = Vector2(1.25,1.25) * [1.25,1.5,1.1,1.5,1.25,1.25,2,2].pick_random() * 1.5
-	pass # Replace with function body.
+	#frame = [0,0,1,1,2,2,3,3,4,5].pick_random()
+	#get_parent().scale = Vector2(1.25,1.25) * [1.25,1.5,1.1,1.5,1.25,1.25,2,2].pick_random() * 1.5
+	
+	if not Engine.is_editor_hint():
+		material.set_shader_parameter("enabled", true)
+		material.set_shader_parameter("mask_offset", DisplayServer.window_get_size()/2.0 - Vector2(0, region_rect.get_center().y) )
+	else:
+		material.set_shader_parameter("enabled", false);
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
